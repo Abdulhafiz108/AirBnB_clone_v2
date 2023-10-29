655b3a71612158ded27f8159ac11c0853862b37c
Abdulhafiz Gazali <ahgazali108@gmail.com>
2023-10-29 12:39:05 -0700

    task 2 fixed

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,20 +9,19 @@ from os.path import exists
 env.hosts = ['35.174.176.83', '54.89.36.249']
 env.user = "ubuntu"
 
-
 def do_deploy(archive_path):
-	"""distributes an archive to the web servers"""
-	if exists(archive_path):
-		file_n = archive_path.split("/")[-1]
-		no_ext = file_n.split(".")[0]
-		path = "/data/web_static/releases/"
-		put(archive_path, '/tmp/')
-		run('mkdir -p {}{}/'.format(path, no_ext))
-		run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-		run('rm /tmp/{}'.format(file_n))
-		run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-		run('rm -rf {}{}/web_static'.format(path, no_ext))
-		run('rm -rf /data/web_static/current')
-		run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
-		return True
-	return False
+    """distributes an archive to the web servers"""
+    if exists(archive_path):
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    return False
